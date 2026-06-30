f36110532f6038cead23173617401c5a7bc7f24f
Denis Antonov <denant-inside@yandex.ru>
2026-06-30 16:10:58 +0300

    Практикуемся в реализации PageObject

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,17 +1,58 @@
 import pytest
 from playwright.sync_api import Page, expect
 
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
 
-    courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text("Courses")
 
-    courses_empty_list_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-    expect(courses_empty_list_title).to_be_visible()
-    expect(courses_empty_list_title).to_have_text("There is no results")
+@pytest.mark.regression
+@pytest.mark.courses
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.check_visible_create_course_form(
+        title="", estimated_time="", description="", max_score="0", min_score="0"
+    )
+
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.fill_create_course_form(
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright",
+        max_score="100",
+        min_score="10"
+    )
+    create_course_page.click_create_course_button()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0,
+        title="Playwright",
+        max_score="100",
+        min_score="10",
+        estimated_time="2 weeks"
+    )
+
+
 
