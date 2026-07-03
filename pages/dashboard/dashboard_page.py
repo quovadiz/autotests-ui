31d594d3b19fce3b90813ca5bd1f23cad175b2e8
Denis Antonov <denant-inside@yandex.ru>
2026-07-03 20:46:59 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/dashboard/dashboard_page.py
+++ pages/dashboard/dashboard_page.py
@@ -13,7 +13,7 @@ class DashboardPage(BasePage):
 
         self.navbar = NavbarComponent(page)
         self.sidebar = SidebarComponent(page)
-        self.toolbar_view = DashboardToolbarViewComponent(page)
+        self.dashboard_toolbar_view = DashboardToolbarViewComponent(page)
         self.students_chart_view = ChartViewComponent(page, "students", "bar")
         self.activities_chart_view = ChartViewComponent(page, "activities", "line")
         self.courses_chart_view = ChartViewComponent(page, "courses", "pie")
