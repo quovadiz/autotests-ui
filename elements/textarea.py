9e73f0cbb9e3c3fc659eaf2a60c40d193c7bed4b
Denis Antonov <denant-inside@yandex.ru>
2026-08-04 13:25:13 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/textarea.py
+++ elements/textarea.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import expect, Locator
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 
@@ -16,6 +17,9 @@ class Textarea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator("textarea").first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"{super().get_raw_locator(nth, **kwargs)}//textarea[1]"
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
 
@@ -23,6 +27,8 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
+
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
@@ -32,3 +38,4 @@ class Textarea(BaseElement):
             logger.info(step)
             expect(locator).to_have_value(value)
 
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
